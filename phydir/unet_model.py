3e69bde9293b19b61d6a2b0eb3a6854461ab03ee
solangii <solangii@unist.ac.kr>
2022-09-21 17:10:04 +0900

    feat: add U-net

---
+++ phydir/unet_model.py
@@ -0,0 +1,53 @@
+""" Full assembly of the parts to form the complete network """
+
+from unet_parts import *
+
+
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, bilinear=False):
+        super(UNet, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.bilinear = bilinear
+
+        self.inc = DoubleConv(n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        factor = 2 if bilinear else 1
+        self.down4 = Down(512, 1024 // factor)
+        self.up1 = Up(1024, 512 // factor, bilinear)
+        self.up2 = Up(512, 256 // factor, bilinear)
+        self.up3 = Up(256, 128 // factor, bilinear)
+        self.up4 = Up(128, 64, bilinear)
+        self.outc = OutConv(64, n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
+
+if __name__ == '__main__':
+    import torch
+    from PIL import Image
+    import numpy as np
+    from torchsummary import summary
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    model = UNet(n_channels=3, n_classes=32).to(device)
+
+    # summary(model, (3, 256, 256))
+    # print(model(torch.randn(1, 3, 256, 256).to(device)).shape)
+
+    img_path= '/home/nsml/phydir/data/celeba_cropped/train/000001.jpg'
+    img = np.array(Image.open(img_path)).transpose(2, 0, 1)
+    img = torch.from_numpy(img).float().unsqueeze(0)
+    print(model(img.to(device)).shape)
