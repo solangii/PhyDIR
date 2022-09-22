112b9aba34b1d636bd0fc5cc45b44ed05d7087f4
solangii <solangii@unist.ac.kr>
2022-09-22 19:43:12 +0900

    feat: add train function

--- phydir/trainer.py
+++ phydir/trainer.py
@@ -29,19 +29,90 @@ class Trainer():
         self.train_loader, self.val_loader, self.test_loader = None, None, None #todo
 
     def load_checkpoint(self, optim=True):
-        pass
+        """Search the specified/latest checkpoint in checkpoint_dir and load the model and optimizer."""
+        if self.checkpoint_name is not None:
+            checkpoint_path = os.path.join(self.checkpoint_dir, self.checkpoint_name)
+        else:
+            checkpoints = sorted(glob.glob(os.path.join(self.checkpoint_dir, '*.pth')))
+            if len(checkpoints) == 0:
+                return 0
+            checkpoint_path = checkpoints[-1]
+            self.checkpoint_name = os.path.basename(checkpoint_path)
+        print(f"Loading checkpoint from {checkpoint_path}")
+        cp = torch.load(checkpoint_path, map_location=self.device)
+        self.model.load_model_state(cp)
+        if optim:
+            self.model.load_optimizer_state(cp)
+        self.metrics_trace = cp['metrics_trace']
+        epoch = cp['epoch']
+        return epoch
 
     def save_checkpoint(self, epoch, optim=True):
-        pass
+        """Save model, optimizer, and metrics state to a checkpoint in checkpoint_dir for the specified epoch."""
+        utils.xmkdir(self.checkpoint_dir)
+        checkpoint_path = os.path.join(self.checkpoint_dir, f'checkpoint{epoch:03}.pth')
+        state_dict = self.model.get_model_state()
+        if optim:
+            optimizer_state = self.model.get_optimizer_state()
+            state_dict = {**state_dict, **optimizer_state}
+        state_dict['metrics_trace'] = self.metrics_trace
+        state_dict['epoch'] = epoch
+        print(f"Saving checkpoint to {checkpoint_path}")
+        torch.save(state_dict, checkpoint_path)
+        if self.keep_num_checkpoint > 0:
+            utils.clean_checkpoint(self.checkpoint_dir, keep_num=self.keep_num_checkpoint)
 
     def save_clean_checkpoint(self, path):
-        pass
+        """Save model state only to specified path."""
+        torch.save(self.model.get_model_state(), path)
 
     def test(self):
         pass
 
     def train(self):
-        pass
+        """Perform training."""
+        ## archive code and configs
+        if self.archive_code:
+            utils.archive_code(os.path.join(self.checkpoint_dir, 'archived_code.zip'), filetypes=['.py', '.yml'])
+        utils.dump_yaml(os.path.join(self.checkpoint_dir, 'configs.yml'), self.cfgs)
+
+        ## initialize
+        start_epoch = 0
+        self.metrics_trace.reset()
+        self.train_iter_per_epoch = len(self.train_loader)
+        self.model.to_device(self.device)
+        self.model.init_optimizers()
+
+        ## resume from checkpoint
+        if self.resume:
+            start_epoch = self.load_checkpoint(optim=True)
+
+        ## initialize tensorboardX logger
+        if self.use_logger:
+            from tensorboardX import SummaryWriter
+            self.logger = SummaryWriter(
+                os.path.join(self.checkpoint_dir, 'logs', datetime.now().strftime("%Y%m%d-%H%M%S")))
+
+            ## cache one batch for visualization
+            self.viz_input = self.val_loader.__iter__().__next__()
+
+        ## run epochs
+        print(f"{self.model.model_name}: optimizing to {self.num_epochs} epochs")
+        for epoch in range(start_epoch, self.num_epochs):
+            self.current_epoch = epoch
+            metrics = self.run_epoch(self.train_loader, epoch)
+            self.metrics_trace.append("train", metrics)
+
+            with torch.no_grad():
+                metrics = self.run_epoch(self.val_loader, epoch, is_validation=True)
+                self.metrics_trace.append("val", metrics)
+
+            if (epoch+1) % self.save_checkpoint_freq == 0:
+                self.save_checkpoint(epoch+1, optim=True)
+            self.metrics_trace.plot(pdf_path=os.path.join(self.checkpoint_dir, 'metrics.pdf'))
+            self.metrics_trace.save(os.path.join(self.checkpoint_dir, 'metrics.json'))
+
+        print(f"Training completed after {epoch+1} epochs.")
 
     def run_epoch(self, loader, epoch=0, is_validation=False, is_test=False):
         pass
