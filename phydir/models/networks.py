ea14460d201dc03cce10aed7987fa414295f1fef
solang-kim <solang.kim@navercorp.com>
2022-10-04 16:49:17 +0900

    fix: fix network parameters (encoder, convlayer)

--- phydir/models/networks.py
+++ phydir/models/networks.py
@@ -9,23 +9,23 @@ class Encoder(nn.Module):
     def __init__(self, cin, cout, nf=64, activation=nn.Tanh):
         super(Encoder, self).__init__()
         network = [
-            nn.Conv2d(cin, nf, kernel_size=4, stride=2, padding=1, bias=False),  # 64x64 -> 32x32
+            nn.Conv2d(cin, nf, kernel_size=8, stride=4, padding=2, bias=False),  # 256x256 -> 64x64
             nn.ReLU(inplace=True),
-            nn.Conv2d(nf, nf*2, kernel_size=4, stride=2, padding=1, bias=False),  # 32x32 -> 16x16
+            nn.Conv2d(nf, nf * 2, kernel_size=8, stride=4, padding=2, bias=False),  # 64x64 -> 16x16
             nn.ReLU(inplace=True),
-            nn.Conv2d(nf*2, nf*4, kernel_size=4, stride=2, padding=1, bias=False),  # 16x16 -> 8x8
+            nn.Conv2d(nf * 2, nf * 4, kernel_size=4, stride=2, padding=1, bias=False),  # 16x16 -> 8x8
             nn.ReLU(inplace=True),
-            nn.Conv2d(nf*4, nf*8, kernel_size=4, stride=2, padding=1, bias=False),  # 8x8 -> 4x4
+            nn.Conv2d(nf * 4, nf * 8, kernel_size=4, stride=2, padding=1, bias=False),  # 8x8 -> 4x4
             nn.ReLU(inplace=True),
-            nn.Conv2d(nf*8, nf*8, kernel_size=4, stride=1, padding=0, bias=False),  # 4x4 -> 1x1
+            nn.Conv2d(nf * 8, nf * 8, kernel_size=4, stride=1, padding=0, bias=False),  # 4x4 -> 1x1
             nn.ReLU(inplace=True),
-            nn.Conv2d(nf*8, cout, kernel_size=1, stride=1, padding=0, bias=False)]
+            nn.Conv2d(nf * 8, cout, kernel_size=1, stride=1, padding=0, bias=False)]
         if activation is not None:
             network += [activation()]
         self.network = nn.Sequential(*network)
 
     def forward(self, input):
-        return self.network(input).reshape(input.size(0),-1)
+        return self.network(input).reshape(input.size(0), -1)
 
 
 class EDDeconv(nn.Module):
@@ -140,12 +140,11 @@ class ConvLayer(nn.Module):
     def __init__(self, cin, cout):
         super(ConvLayer, self).__init__()
         network = [
-            nn.Conv2d(cin, 64, kernel_size=4, stride=2, padding=1, bias=False),
+            nn.Conv2d(cin, cout, kernel_size=1, stride=1, padding=0, bias=False),
         ]
         self.network = nn.Sequential(*network)
 
     def forward(self, input):
-        # input : N*32*256*256 -> 1*32*256*256
         return self.network(input)
 
 
@@ -153,10 +152,12 @@ if __name__ == '__main__':
     from torchsummary import summary
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    input = torch.randn(6, 32, 256, 256).to(device)
+    input = torch.randn(1, 32, 256, 256).to(device)
 
-    network = ConvLayer(32, 64).to(device)
+    network = ConvLayer(32, 32).to(device)
     summary(network, (32, 256, 256))
+    output = network(input)
+    print(output.shape)
 
     # depth network
     # netD = EDDeconv(cin=3, cout=1, nf=256, zdim=512, activation=None).to(device) # original params: nf->64, zdim->256
@@ -171,7 +172,7 @@ if __name__ == '__main__':
     # print(output.shape)
 
     # light network
-    # netL = Encoder(cin=3, cout=4, nf=32).to(device)
+    # netL = Encoder(cin=3, cout=2, nf=32).to(device)
     # summary(netL, (3, 256, 256))
     # output = netL(input)
     # print(output.shape)
