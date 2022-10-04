c33fc62610d601811967d67066d6f1b57ce26309
solang-kim <solang.kim@navercorp.com>
2022-10-04 16:49:53 +0900

    feat: update `model.py`

--- phydir/model.py
+++ phydir/model.py
@@ -4,9 +4,9 @@ import glob
 import torch
 import torch.nn as nn
 import torchvision
-from .models import EDDeconv, Encoder, UNet
+from .models import EDDeconv, Encoder, UNet, ConvLayer
 from . import utils
-# from .renderer import Renderer
+from .renderer import Renderer
 
 
 EPS = 1e-7
@@ -23,15 +23,19 @@ class PhyDIR():
         self.max_amb_light = cfgs.get('max_amb_light', 1.)
         self.min_diff_light = cfgs.get('min_diff_light', 0.)
         self.max_diff_light = cfgs.get('max_diff_light', 1.)
+        self.xyz_rotation_range = cfgs.get('xyz_rotation_range', 60)
+        self.xy_translation_range = cfgs.get('xy_translation_range', 0.1)
+        self.z_translation_range = cfgs.get('z_translation_range', 0.1)
+
         self.lr = cfgs.get('lr', 1e-4)
-        # self.renderer = Renderer(cfgs)
+        self.renderer = Renderer(cfgs)
 
         ## networks and optimizers
         self.netD = EDDeconv(cin=3, cout=1, nf=256, zdim=512, activation=None)
-        self.netL = Encoder(cin=3, cout=4, nf=32)
+        self.netL = Encoder(cin=3, cout=2, nf=32)
         self.netV = Encoder(cin=3, cout=6, nf=32)
         self.netT = UNet(n_channels=3, n_classes=32)
-        self.renderer = UNet(n_channels=32, n_classes=3) # todo check shape
+        self.netC = ConvLayer(cin=32, cout=32)
 
         self.network_names = [k for k in vars(self) if 'net' in k]
         self.make_optimizer = lambda model: torch.optim.Adam(
@@ -79,9 +83,9 @@ class PhyDIR():
         self.device = device
         for net_name in self.network_names:
             setattr(self, net_name, getattr(self, net_name).to(device))
-        if self.other_param_names:
-            for param_name in self.other_param_names:
-                setattr(self, param_name, getattr(self, param_name).to(device))
+        # if self.other_param_names:
+        #     for param_name in self.other_param_names:
+        #         setattr(self, param_name, getattr(self, param_name).to(device))
 
     def set_train(self):
         for net_name in self.network_names:
@@ -111,21 +115,64 @@ class PhyDIR():
 
     def forward(self, batch):
         # [data, data, data, ...., data]
-        # data: K, 3, 256, 256 (K is random number with 1~6)
         loss_total = 0
         for input in batch:
+            # data: K, 3, 256, 256 (K is random number with 1~6)
+            input = torch.stack(input, dim=0)
             self.input_im = input.to(self.device) *2.-1.
             k, c, h, w = self.input_im.shape
 
-            # depth
-            # view
-            # light
-            # texture
+            # 3D Networks
+            # 1. predict depth
+            self.canon_depth_raw = self.netD(self.input_im).squeeze(1)
+            self.canon_depth = self.canon_depth_raw - self.canon_depth_raw.view(k, -1).mean(1).view(k, 1, 1)
+            self.canon_depth = self.canon_depth.tanh()
+            self.canon_depth = self.depth_rescaler(self.canon_depth)
 
-            # shading
+            # 2. predict light
+            canon_light = self.netL(self.input_im)  # Bx2
+            self.canon_light_a = self.amb_light_rescaler(canon_light[:, :1])  # ambience term
+            self.canon_light_b = self.diff_light_rescaler(canon_light[:, 1:])  # diffuse term
+            canon_light_dxy = canon_light[:, 2:]
+            self.canon_light_d = torch.cat([canon_light_dxy, torch.ones(k * 2, 1).to(self.input_im.device)], 1)
+            self.canon_light_d = self.canon_light_d / (
+                (self.canon_light_d ** 2).sum(1, keepdim=True)) ** 0.5  # diffuse light direction
+
+            # 3. predict viewpoint
+            self.view = self.netV(self.input_im).repeat(2, 1)
+            self.view = torch.cat([
+                self.view[:, :3] * math.pi / 180 * self.xyz_rotation_range,
+                self.view[:, 3:5] * self.xy_translation_range,
+                self.view[:, 5:] * self.z_translation_range], 1)
+
+            # shading (light + shape, lambertian function)
+            self.canon_normal = self.renderer.get_normal_from_depth(self.canon_depth)
+            t1 = self.canon_light_d.view(-1, 1, 1, 3)
+            t2 = self.canon_normal * self.canon_light_d.view(-1, 1, 1, 3)
+            t3 = (self.canon_normal * self.canon_light_d.view(-1, 1, 1, 3)).sum(3)
+            t4 = (self.canon_normal * self.canon_light_d.view(-1, 1, 1, 3)).sum(3).clamp(
+                min=0)
+            t5 = (self.canon_normal * self.canon_light_d.view(-1, 1, 1, 3)).sum(3).clamp(
+                min=0).unsqueeze(1)
+            self.canon_diffuse_shading = (self.canon_normal * self.canon_light_d.view(-1, 1, 1, 3)).sum(3).clamp(
+                min=0).unsqueeze(1)
+            canon_shading = self.canon_light_a.view(-1, 1, 1, 1) + self.canon_light_b.view(-1, 1, 1,
+                                                                                           1) * self.canon_diffuse_shading
+
+            # predict texture
+            self.textures = None
+            self.cannon_texture = None
+
             # multi-image-shading
-            # rasterization
-            # grid-sampling
+            self.shaded_texture = None
+            self.shaded_cannon_texture = None
+
+            # reconstruction
+            self.renderer.set_transform_matrices(self.view)
+            self.recon_depth = self.renderer.warp_canon_depth(self.canon_depth)
+            self.recon_normal = self.renderer.get_normal_from_depth(self.recon_depth)
+            grid_2d_from_canon = self.renderer.get_inv_warped_2d_grid(self.recon_depth)
+            self.recon_im = nn.functional.grid_sample(self.canon_im, grid_2d_from_canon, mode='bilinear') # todo cannon_im -> texture
 
             # rendering
 
