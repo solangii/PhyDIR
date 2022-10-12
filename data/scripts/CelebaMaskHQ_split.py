b89d491ee3926091d49b3239bda41c3d9f59ca11
solang-kim <solang.kim@navercorp.com>
2022-10-13 01:35:13 +0900

    chore: fix uncorrect parts

--- data/scripts/CelebaMaskHQ_split.py
+++ data/scripts/CelebaMaskHQ_split.py
@@ -4,15 +4,16 @@ import numpy as np
 
 path = '/home/nsml/phydir/data'
 id_list = np.loadtxt(os.path.join(path, 'ANNOTATION', 'identity_CelebA.txt'), dtype='str')
-mapping_list = np.loadtxt(os.path.join(path, 'ORIGINAL', 'CelebAMask-HQ', 'CelebAMask-HQ-to-CelebA-mapping.txt'), dtype='str')
+mapping_list = np.loadtxt(os.path.join(path, 'ORIGINAL', 'CelebAMask-HQ', 'CelebA-HQ-to-CelebA-mapping.txt'), dtype='str')
 
 id_dict = {} # key: jpg, value: id
 for row in id_list:
     id_dict[row[0]] = row[1]
 
 mapping_dict = {} # key: CelebAMask-HQ idx, value: CelebA idx ( 0 - 119614.jpg)
+mapping_list = mapping_list[1:] # remove header
 for row in mapping_list:
-    mapping_dict[row[0]] = row[2] #todo: check if it is correct, and first row is header
+    mapping_dict[row[0]] = row[2]
 
 
 celeba_dict = {} # key: id, value: list of jpg
@@ -45,26 +46,29 @@ datalist = {
     'val': {},
     'test': {}
 }
-
 # random select dir
-for phase in phase.keys():
-    for i in range(phase[phase]):
+for p in phase.keys():
+    for i in range(phase[p]):
         # logger
         if i % 100 == 0:
-            print(f'{phase} {i+1}/{phase[phase]}')
+            print(f'{p} {i+1}/{phase[p]}')
 
         # get id, list of jpg
         id, img_list = celeba_dict.popitem()
 
         # copy to target path
         for img in img_list:
-            target_path = os.path.join(path, 'celebamask_hq', phase)
+            target_path = os.path.join(path, 'celebamask_hq', p)
             os.system(f'cp {data_path}/{img} {target_path}/{img}')
         # add datalist
         if len(img_list) >= 6:
-            datalist[phase][id] = img_list
+            datalist[p][id] = img_list
 
 # save datallist to json file
-for phase in datalist.keys():
-    with open(f'{path}/celebamask_hq/{phase}/datalist/combine.json', 'w') as f:
-        json.dump(datalist[phase], f)
+for p in datalist.keys():
+    with open(f'{path}/celebamask_hq/{p}/datalist/combine.json', 'w') as f:
+        json.dump(datalist[p], f)
+
+for p in datalist.keys():
+    print(f'{p} id: {len(datalist[p])}')
+    print(f'{p} img: {sum([len(datalist[p][id]) for id in datalist[p].keys()])}')
